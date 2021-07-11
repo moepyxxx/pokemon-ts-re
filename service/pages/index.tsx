d232fc571142724d4bb54b53d35bb1298957a19f
moepyxxx <sjpy_xx_1007@docomo.ne.jp>
2021-07-11 10:19:41 +0900

    ポケモン図鑑に状態を持たせてクリック時API処理＋再レンダーするようにした

--- service/pages/index.tsx
+++ service/pages/index.tsx
@@ -3,15 +3,17 @@ import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
 import { BookPokemon } from '../types/pokemon/BookPokemon'
 import { useState } from 'react'
 
-export default function Home({ pokemons }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+export default function Home({ initialPokemons }: InferGetServerSidePropsType<typeof getServerSideProps>) {
 
   const [pager, setPager] = useState<number>(1);
+  const [pokemons, setPokemons] = useState<BookPokemon[]>(initialPokemons);
 
   const more = async () => {
     setPager(pager + 1);
-    const result = await fetch (`http://localhost:3000/api/bookpokemon/?offset=${pager * 20}&limit=20`).then(res => res.json());
-    console.log(result);
+    const result: BookPokemon[] = await fetch (`http://localhost:3000/api/bookpokemon/?offset=${pager * 20}&limit=20`).then(res => res.json());
+    setPokemons(pokemons.concat(result));
   }
+
   return (
     <section>
       <h1>ポケモン図鑑</h1>
@@ -27,6 +29,6 @@ export default function Home({ pokemons }: InferGetServerSidePropsType<typeof ge
 }
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  const pokemons: BookPokemon[] = await fetch ('http://localhost:3000/api/bookpokemon').then(res => res.json());
-  return { props: { pokemons } };
+  const initialPokemons: BookPokemon[] = await fetch ('http://localhost:3000/api/bookpokemon').then(res => res.json());
+  return { props: { initialPokemons } };
 }
