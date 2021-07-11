73f946ce8f2813c45a935a17933042cbc661c455
moepyxxx <sjpy_xx_1007@docomo.ne.jp>
2021-07-11 17:44:16 +0900

    プロトコル部分を追加した

--- service/pages/index.tsx
+++ service/pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home({ initialPokemons, isNext }: InferGetServerSideProp
 
   const more = async () => {
     setPager(pager + 1);
-    const { result, isNext } = await fetch (`${process.env.NEXT_PUBLIC_VERCEL_URL}/api/bookpokemon/?offset=${pager * 20}&limit=20`).then(res => res.json());
+    const { result, isNext } = await fetch (`${process.env.NEXT_PUBLIC_PROTOCOL}${process.env.NEXT_PUBLIC_VERCEL_URL}/api/bookpokemon/?offset=${pager * 20}&limit=20`).then(res => res.json());
     setPokemons(pokemons.concat(result));
     setNext(isNext);
   }
@@ -32,7 +32,7 @@ export default function Home({ initialPokemons, isNext }: InferGetServerSideProp
 
 export const getServerSideProps: GetServerSideProps = async () => {
 
-  const { result, isNext } = await fetch (`${process.env.NEXT_PUBLIC_VERCEL_URL}/api/bookpokemon`).then(res => res.json());
+  const { result, isNext } = await fetch (`${process.env.NEXT_PUBLIC_PROTOCOL}${process.env.NEXT_PUBLIC_VERCEL_URL}/api/bookpokemon`).then(res => res.json());
   return {
     props: {
       initialPokemons: result,
