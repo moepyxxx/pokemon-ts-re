a7a44d98f3d26d000978c4e002a1db34deb5b671
moepyxxx <sjpy_xx_1007@docomo.ne.jp>
2021-07-18 21:19:46 +0900

    場所によってゲットできるポケモンを変更できるようにした

--- service/lib/pokemon/searchPokemon.ts
+++ service/lib/pokemon/searchPokemon.ts
@@ -1,4 +1,6 @@
 import MAX_POKEMON from '../database/maxPokemon';
+import TYPE_NAME from '../database/typeName';
+import { POKEMON_URL } from './url';
 import getPokemonImage from './getPokemonImage';
 import getPokemonName from './getPokemonName';
 
@@ -9,11 +11,49 @@ type Result = {
   id?: number;
 }
 
-const searchPokemon = async () : Promise<Result> => {
-  const id = 1 + Math.floor( Math.random() * MAX_POKEMON );
-  
-  const name = await getPokemonName(id.toString());
-  const image = await getPokemonImage(id.toString());
+const searchPokemon = async (ids: number[]) : Promise<Result> => {
+
+  let isSet = false;
+  let randomId;
+  let fixedId;
+  do {
+    randomId = 1 + Math.floor( Math.random() * MAX_POKEMON );
+    fixedId = await fetch (POKEMON_URL + randomId)
+
+      .then(res => res.json())
+      .then(res => {
+
+        // 表示させたポケモンのタイプ名を取得
+        const typeNames: string[] = res.types.map(types => {
+          return types.type.name;
+        });
+
+        // ポケモンのタイプがその場所に出現するポケモンのタイプと合致したら結果を返却する
+        const result = typeNames.some(name => {
+
+          let isFixed = false;
+          for (let i = 0; i < ids.length; i++) {
+            if (TYPE_NAME[ids[i]].en === name) {
+              isFixed = true;
+              break;
+            }
+          }
+          return isFixed;
+        });
+
+        // 結果がtrueならdo..whileのループを抜ける
+        if (result) {
+          isSet = true;
+        }
+
+        // 決まったポケモンのIDをかえす
+        return res.id;
+      })
+    ;
+  } while (isSet === false);
+
+  const name = await getPokemonName(fixedId);
+  const image = await getPokemonImage(fixedId);
 
   if (!name || !image) return { result: false };
 
@@ -21,7 +61,7 @@ const searchPokemon = async () : Promise<Result> => {
     result: true,
     name,
     image,
-    id
+    id: fixedId
   };
 }
 export default searchPokemon;
