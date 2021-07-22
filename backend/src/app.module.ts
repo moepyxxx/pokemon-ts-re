792c01988cf15b993831e8c5496492c78256917a
moepyxxx <sjpy_xx_1007@docomo.ne.jp>
2021-07-22 17:01:38 +0900

    types モック追加

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,10 +5,12 @@ import { BookPokemonsController } from './book-pokemons/book-pokemons.controller
 import { BookPokemonsService } from './book-pokemons/book-pokemons.service';
 import { SearchplacesController } from './searchplaces/searchplaces.controller';
 import { SearchplacesService } from './searchplaces/searchplaces.service';
+import { TypesController } from './types/types.controller';
+import { TypesService } from './types/types.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, BookPokemonsController, SearchplacesController],
-  providers: [AppService, BookPokemonsService, SearchplacesService],
+  controllers: [AppController, BookPokemonsController, SearchplacesController, TypesController],
+  providers: [AppService, BookPokemonsService, SearchplacesService, TypesService],
 })
 export class AppModule {}
