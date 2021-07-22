2fc55f687ad84c5c1bba7d9170015e692c5b813b
moepyxxx <sjpy_xx_1007@docomo.ne.jp>
2021-07-22 11:20:57 +0900

    bookPokemonのレスポンスが変えるようにした

--- backend/src/book-pokemons/book-pokemons.controller.ts
+++ backend/src/book-pokemons/book-pokemons.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { ISummaryBookPokemons, IBookPokemons } from './interface';
 
 @Controller('book-pokemons')
-export class BookPokemonsController {}
+export class BookPokemonsController {
+  @Get()
+  findSummaryAll(): ISummaryBookPokemons[] {
+    return [{
+      id: 1,
+      name: 'フシギダネ',
+      image: 'string-image-1',
+      type: [{
+        id: 3,
+        name: 'くさ'
+      }]
+    }, {
+      id: 2,
+      name: 'フシギソウ',
+      image: 'string-iamge-2',
+      type: [{
+        id: 3,
+        name: 'くさ'
+      }]
+    }];
+  }
+
+  @Get(':id')
+  findDetailOne(@Param() params): IBookPokemons {
+    return {
+      id: params.id,
+      name: 'フシギダネ',
+      image: 'string-image-1',
+      type: [{
+        id: 3,
+        name: 'くさ'
+      }],
+      abilities: {
+        name: 'のうりょく',
+        description: 'こんなのうりょくである'
+      },
+      height: 30,
+      weight: 30,
+      species: 'しゅのとくちょうがあります'
+    }
+  }
+}
