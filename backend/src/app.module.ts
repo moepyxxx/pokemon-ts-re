57c483681f60c25e32a61892c03e587aa19387bd
moepyxxx <sjpy_xx_1007@docomo.ne.jp>
2021-07-24 12:03:19 +0900

    module整理して環境変数読み込む準備した

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,14 +5,16 @@ import { SearchplacesController } from './searchplaces/searchplaces.controller';
 import { SearchplacesService } from './searchplaces/searchplaces.service';
 import { TypesController } from './types/types.controller';
 import { TypesService } from './types/types.service';
-import { NewsController } from './news/news.controller';
-import { NewsService } from './news/news.service';
 import { BookPokemonsModule } from './book-pokemons/book-pokemons.module';
-import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
+import { NewsModule } from './news/news.module';
 
 @Module({
-  imports: [BookPokemonsModule, HttpModule],
-  controllers: [AppController, SearchplacesController, TypesController, NewsController],
-  providers: [AppService, SearchplacesService, TypesService, NewsService],
+  imports: [BookPokemonsModule, NewsModule, ConfigModule.forRoot({
+    isGlobal: true,
+    envFilePath: ['.env']
+  }), NewsModule],
+  controllers: [AppController, SearchplacesController, TypesController],
+  providers: [AppService, SearchplacesService, TypesService],
 })
 export class AppModule {}
